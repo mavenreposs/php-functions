8abb494c63a8ee9c0e1e6a4e81eb32091b8fb9dc
王正东 <wangzhengdong@ecmoban.com>
2021-06-02 13:57:53 +0800

    添加测试用例

--- src/test/java/io/github/mavenreposs/php/functions/Test_PHPFunctions.java
+++ src/test/java/io/github/mavenreposs/php/functions/Test_PHPFunctions.java
@@ -1,7 +1,11 @@
 package io.github.mavenreposs.php.functions;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Test_PHPFunctions {
 
@@ -16,6 +20,20 @@ public class Test_PHPFunctions {
     public void test_getdate() {
         GetDate getDate = PHPFunctions.getdate();
         System.out.println(getDate);
+    }
+
+    @Test
+    public void test_md5() {
+        String hash = PHPFunctions.md5("123123");
+        System.out.println(hash);
+        Assertions.assertEquals("4297f44b13955235245b2497399d7a93", hash);
+    }
+
+    @Test
+    public void test_sha1() {
+        String hash = PHPFunctions.sha1("123123");
+        System.out.println(hash);
+        Assertions.assertEquals("601f1889667efaebb33b8c12572835da3f027f78", hash);
     }
 
 
